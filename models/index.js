05620710f40692da5f8e7832c8a68779373950b2
Talles Henrique <talles.hvieira@gmail.com>
2020-07-14 18:33:51 -0300

    projeto com alterações modelo,rotas e controle

---
+++ models/index.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+import gradeModel from './gradesModel.js';
+
+const db = {};
+
+db.mongoose = mongoose;
+db.url = process.env.MONGODB;
+db.grade = gradeModel(mongoose);
+
+export { db };
